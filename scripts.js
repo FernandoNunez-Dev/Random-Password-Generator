636cac3ec2d47d0a725959e5e76cb52de73f9cb9
Dev3 <Fernandonunez0304@gmail.com>
2020-10-06 00:16:43 -0400

    updated scripts.js file

--- scripts.js
+++ scripts.js
@@ -1,15 +1,29 @@
 
-
-function randomPassword(length) {
-    var chars = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()-+<>ABCDEFGHIJKLMNOP1234567890";
-    var pass = "";
-    for (var x = 0; x < length; x++) {
-        var i = Math.floor(Math.random() * chars.length);
-        pass += chars.charAt(i);
+    let randomPassword = () => {
+        let slide = document.getElementById("slide").value;
+        let pass = document.getElementById("password");
+        let chars = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+        let password = "";
+        for ( var x = 0; x <= slide; x++ ) {
+            password = password + chars.charAt(Math.floor(Math.random() * Math.floor(chars.length - 1)));
+            pass.value = password;
+        }
     }
-    return pass;
-}
 
-function generate() {
-    myform.row_password.value = randomPassword(myform.length.value);
+let lengthy = document.getElementById("length");
+lengthy.innerHTML = "Length: 25";
+  
+slide.oninput = () => {
+    if ( slide.value > 0 ) {
+        lengthy.innerHTML = `Length: ${slide.value}`
+    } else {
+        lengthy.innerHTML = "Length: 1";
+    }
+}
+  
+let copyPassword = () => {
+    let pass = document.getElementById("password");
+    pass.select();
+    document.execCommand("Copy");
+    alert("Password copied to clipboard!");
 }
